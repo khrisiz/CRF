c43e858b193b7c5d63d8d5940ad2cc4d7b55201a
khrisiz <christophercarew23@gmail.com>
2025-06-20 02:24:50 -0500

    Update server.js

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ io.on('connection', socket => {
 });
 
 app.use(express.static('public'));
-
-server.listen(3000, () => {
-  console.log('Server running on http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
