5cafa16526733ca994679e678efcd495ac20c807
khrisiz <christophercarew23@gmail.com>
2025-06-22 16:10:10 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
+const path = require('path');
+
 
 const app = express();
 app.use(express.static(path.join(__dirname, 'public'))); // ✅ this is correct
