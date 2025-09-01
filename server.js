78cb6f36533ba0025248c9b63c8e43f4a57ae267
khrisiz <christophercarew23@gmail.com>
2025-09-01 01:21:27 -0500

    Update server.js

--- server.js
+++ server.js
@@ -153,6 +153,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log(`🚀 Server running on port: http://localhost:3000`);
+  console.log(`🚀 Server running on port: ${PORT}`);
 });
 
