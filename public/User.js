7b851c9ab8d5928e8ea4be1fe693c9fd380fa528
khrisiz <christophercarew23@gmail.com>
2025-08-31 23:29:29 -0500

    Delete public directory

--- public/User.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const userSchema = new mongoose.Schema({
-  username: { type: String, unique: true, required: true },
-  password: { type: String, required: true },
-  gender: { type: String, enum: ['male', 'female'], required: true },
-});
-
-module.exports = mongoose.model('User', userSchema);
