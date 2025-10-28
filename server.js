0fa383bdb24f50088754d818b71876b09b7a645e
khrisiz <christophercarew23@gmail.com>
2025-10-27 21:00:22 -0500

    Refactor comments and improve socket connection handling

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// server.js
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
@@ -7,25 +8,25 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-// Serve static files
+// Serve static files from public folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Optional root route
+// Root route
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 // WebRTC signaling
-io.on('connection', socket => {
+io.on('connection', (socket) => {
   console.log('User connected');
 
-  socket.on('offer', offer => socket.broadcast.emit('offer', offer));
-  socket.on('answer', answer => socket.broadcast.emit('answer', answer));
-  socket.on('ice-candidate', candidate => socket.broadcast.emit('ice-candidate', candidate));
+  socket.on('offer', (offer) => socket.broadcast.emit('offer', offer));
+  socket.on('answer', (answer) => socket.broadcast.emit('answer', answer));
+  socket.on('ice-candidate', (candidate) => socket.broadcast.emit('ice-candidate', candidate));
 
   socket.on('disconnect', () => console.log('User disconnected'));
 });
 
-// Use Render's dynamic PORT
+// Listen on Render's dynamic PORT
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
